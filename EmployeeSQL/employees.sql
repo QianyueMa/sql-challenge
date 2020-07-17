e61005ddadd5034b9a28ddc14ddc34db7be57cde
QianyueMa <amandama215@gmail.com>
2020-07-16 22:27:20 -0400

    .

---
+++ EmployeeSQL/employees.sql
@@ -0,0 +1,12 @@
+-- Part I: Data Engineering --
+
+-- First drop the tables if exist (according to the csv files)
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+-- Create the tables
+
